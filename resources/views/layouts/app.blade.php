fa9daef0a985418cf3e79b32f00efdf9204dfebe
細川俊介 <>
2020-10-07 22:58:54 +0900

    first

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', '時間割作成') }}</title>
+    <title>時間割作成</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
